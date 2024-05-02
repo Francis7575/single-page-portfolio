5d942be6bb2976fb2469cdfb9ce1371b4ce43a8c
Francis7575 <francis.albert.2009@gmail.com>
2024-05-02 14:37:44 -0700

    updated

--- react-single-page-portfolio/src/components/GalleryNavigation.tsx
+++ react-single-page-portfolio/src/components/GalleryNavigation.tsx
@@ -9,13 +9,13 @@ type GalleryNavigationProps = {
 }
 
 const GalleryNavigation = ({ images, currentIndex, setCurrentIndex }: GalleryNavigationProps) => {
-    
+
     const nextImage = () => {
         setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
     };
 
     const previousImage = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? 4 : prevIndex - 1));
     };
 
     useEffect(() => {
@@ -24,12 +24,18 @@ const GalleryNavigation = ({ images, currentIndex, setCurrentIndex }: GalleryNav
 
     return (
         <div className='flex justify-center gap-4 mt-[1.81rem] md:mt-[3.5rem]'>
-            <button className='bg-black py-[1.37rem] min-w-[4rem] flex justify-center rounded-[4rem] hover:bg-galatic-blue'
-                onClick={previousImage}>
+            <button
+                className='bg-black py-[1.37rem] min-w-[4rem] flex justify-center rounded-[4rem] hover:bg-galatic-blue'
+                onClick={previousImage}
+                aria-label="Previous Image"
+                title="Previous Image">
                 <img src={leftArrow} alt="Previous Image" />
             </button>
-            <button className='bg-black py-[1.37rem] min-w-[4rem] flex justify-center rounded-[4rem] hover:bg-galatic-blue'
-                onClick={nextImage}>
+            <button
+                className='bg-black py-[1.37rem] min-w-[4rem] flex justify-center rounded-[4rem] hover:bg-galatic-blue'
+                onClick={nextImage}
+                aria-label="Next Image"
+                title="Next Image">
                 <img src={rightArrow} alt="Next Image" />
             </button>
         </div>
